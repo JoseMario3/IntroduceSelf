acd2507dfe805ea0262a4cbb64c60c7fae280568
Jose Folgar <f.jose@wustl.edu>
2024-01-18 23:19:16 -0600

    Continued working on project

--- IntroduceStudent/IntroduceStudent/ViewController.swift
+++ IntroduceStudent/IntroduceStudent/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var schoolNameTextField: UITextField!
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
     @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var colorNameTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +32,16 @@ class ViewController: UIViewController {
     @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
         
-        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and my favorite color is \(colorNameTextField.text!). I own \(numberOfPetsLabel.text!) bunny, and it is \(morePetsSwitch.isOn) that I want more pets."
         
         print(introduction)
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         
+        let action = UIAlertAction(title: "Nice To Meet You!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
     }
+    
 }
